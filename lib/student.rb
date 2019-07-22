83bfe48be9b821dd7cb3f67fadd5ee689a20c70f
Kateryna Strough <katerynastrough@hotmail.com>
2019-07-22 23:53:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,16 @@
 class Student
 
+attr_accessor :name, :grade
+attr_reader :id 
+
+def initialize(name, grade, id=nil)
+  @id = id 
+  @name = name 
+  @ grade = grade 
+end 
+
+
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
