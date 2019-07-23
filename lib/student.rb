a216c2262d437352cf7ab7665361ec560382d34c
Kateryna Strough <katerynastrough@hotmail.com>
2019-07-23 00:02:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,14 @@ def self.create_table
  DB[:conn].execute(sql)
  end
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+def self.drop_table
+sql = <<- SQL
+DROP TABLE students
+SQL
+
+DB[:conn].execute(sql)
+
+end
+  
 
 end
